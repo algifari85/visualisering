1e66ac80bb7b25cb27189e619a55e6979d2a40c2
algifari85 <andreas-sundstrom@hotmail.com>
2018-03-18 11:37:55 +0100

    Justeringar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-var ctx = document.getElementById("myChart").getContext('2d');
+var ctx1 = document.getElementById("myChart1").getContext('2d');
+var ctx2 = document.getElementById("myChart2").getContext('2d');
 
 $.get('../data/kontaktergrupperade.csv',function(d){
 	var data = Papa.parse(d,{
@@ -17,7 +18,7 @@ $.get('../data/kontaktergrupperade.csv',function(d){
 		vardtyngd.data.antalkontakter.push(e['Antal kontakter'])
 	})
 	console.log(vardtyngd);
-	var myChart = new Chart(ctx, {
+	var myChart = new Chart(ctx1, {
 		type:'bar',
 		data: {
 			labels: vardtyngd.labels,
@@ -25,29 +26,113 @@ $.get('../data/kontaktergrupperade.csv',function(d){
 					label: 'Vårdtyngd',
 					data: vardtyngd.data.vardtyngd,
 					backgroundColor: 
-							'rgba(255, 99, 132, 0.5)'
+							'rgba(255, 99, 132, 1)'
 
 					,
 					borderColor: 
 							'rgba(255,99,132,1)'
 
 					,
-					borderWidth: 1,
+
 					yAxisID: 'left-y-axis'
 			},{
 				label: 'Antal kontakter',
 				data: vardtyngd.data.antalkontakter,
 				backgroundColor: 
-						'rgba(0,0, 235, 0.5)',
+						'rgba(0,0, 235, 1)',
 				borderColor: 
 						'rgba(0, 0, 235, 1)'
 			,
-				borderWidth: 1,
 				yAxisID: 'right-y-axis'
 		}],
 	},
 	options: {
+
 		scales: {
+
+			yAxes: [{
+					id: 'left-y-axis',
+					type: 'linear',
+					position: 'left',
+					ticks: {
+						suggestedMin: 0
+					},
+					scaleLabel:{
+						display:true,
+						labelString: 'Vårdtyngd'
+					},
+					gridLines:{
+						display:true
+					}
+			}, {
+					id: 'right-y-axis',
+					type: 'linear',
+					position: 'right',
+
+					ticks:{
+						suggestedMin:0
+					},
+					gridLines:{
+						display:false
+					},
+					scaleLabel:{
+						display:true,
+						labelString: 'Antal kontakter'
+					},
+			}]
+	}
+	}
+	})
+});
+$.get('../data/kontaktergrupperade2.csv',function(d){
+	var data = Papa.parse(d,{
+		header:true
+	});
+	console.log(data.data);
+	var vardtyngd = {};
+	vardtyngd.labels = [];
+	vardtyngd.data = {};
+	vardtyngd.data.vardtyngd = [];
+	vardtyngd.data.antalkontakter = []
+	data.data.forEach(function(e){
+		vardtyngd.labels.push(e.Team)
+		
+		vardtyngd.data.vardtyngd.push(parseInt(e['Omvårdnadstyngd'])+parseInt(e['Medicinsk tyngd']))
+		vardtyngd.data.antalkontakter.push(e['Antal kontakter'])
+	})
+	console.log(vardtyngd);
+	var myChart = new Chart(ctx2, {
+		type:'bar',
+		data: {
+			labels: vardtyngd.labels,
+			datasets: [{
+					label: 'Vårdtyngd',
+					data: vardtyngd.data.vardtyngd,
+					backgroundColor: 
+							'rgba(255, 99, 132, 1)'
+
+					,
+					borderColor: 
+							'rgba(255,99,132,1)'
+
+					,
+
+					yAxisID: 'left-y-axis'
+			},{
+				label: 'Antal kontakter',
+				data: vardtyngd.data.antalkontakter,
+				backgroundColor: 
+						'rgba(0,0, 235, 1)',
+				borderColor: 
+						'rgba(0, 0, 235, 1)'
+			,
+				yAxisID: 'right-y-axis'
+		}],
+	},
+	options: {
+
+		scales: {
+
 			yAxes: [{
 					id: 'left-y-axis',
 					type: 'linear',
